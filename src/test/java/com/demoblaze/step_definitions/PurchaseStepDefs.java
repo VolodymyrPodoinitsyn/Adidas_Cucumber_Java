603bc57769ba375376d5902b1849d02db40a199b
VolodymyrPodoinitsyn <podoinitsynvladimir@gmail.com>
2021-07-18 22:45:10 -0700

    second commit productAdder

--- src/test/java/com/demoblaze/step_definitions/PurchaseStepDefs.java
+++ src/test/java/com/demoblaze/step_definitions/PurchaseStepDefs.java
@@ -8,7 +8,7 @@ public class PurchaseStepDefs {
 
     @Given("User is on the Home Page")
     public void user_is_on_the_home_page() {
-        Driver.getDriver(ConfigurationReader.getProperty("url"));
+        Driver.getDriver().get(ConfigurationReader.getProperty("url"));
     }
 
 
